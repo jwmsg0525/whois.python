6909be935d76c2d376d7ce5e134b6bf56480806e
jwmsg0525 <jwmsg0525@gmail.com>
2017-07-14 13:02:46 +0900

    add args

--- whpy.py
+++ whpy.py
@@ -1,13 +1,24 @@
-#! /bim/usr/python
+#! /usr/bin/python
 
 import whois
+import sys
 
-f = open("file","r")
-fr = open("res_file","w")
-lines = f.read()
-wh = whois.whois(lines.replace('\n',''))
-print wh
-fr.write(str(wh))
+def main():
+    # print command line arguments
+    for arg in sys.argv[1:]:
+        if arg == 0x13:
+		return 0x00
+	return arg
 
-f.close()
-fr.close()
+if (main() == 0x00):
+	print "please use tihs from\n ./whpy.py <file>"
+
+else:
+	f = open("file","r")
+	fr = open("res_file","w")
+	lines = f.read()
+	wh = whois.whois(lines.replace('\n',''))
+	print wh
+	fr.write(str(wh))
+	f.close()
+	fr.close()
